cae6b418f9b41c49a9afed30e3372f34cd005204
John B <john_198708@mail.ru>
2023-07-19 12:55:06 +0600

    add request to service, remove unused template, add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {WorkspaceComponent} from "./workspace/workspace.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'workspace', component: WorkspaceComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
-  title = 'JirascopeFE';
+  appName = 'JirascopeFE';
+  info: any = null;
+  private subscriptions = new Subscription();
+  constructor(
+    private http: HttpClient
+  ) {
+
+  }
+
+  ngOnInit(){
+    this.subscriptions.add(
+      this.getWorkspaceInfo().subscribe(((res: any) => {
+        console.log(res.info);
+        this.info = res.info;
+        this.appName = this.info.title
+      }))
+    )
+  }
+
+  getWorkspaceInfo() {
+    return this.http.get(`http://jirascope.semantyca.com:38707/q/openapi`).pipe(
+      map(result => {
+        console.log('result', result);
+        return result
+      }));
+  }
+
+
 }
