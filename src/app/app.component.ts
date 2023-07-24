50a8d2ceb0dbf2486ff2b1a4a2873b81f12f7b97
John B <john_198708@mail.ru>
2023-07-24 16:15:36 +0600

    added MatButtonModule added fakeAuthFunc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {map, Subscription} from 'rxjs';
 
 @Component({
@@ -9,9 +9,10 @@ import {map, Subscription} from 'rxjs';
 })
 
 
-export class AppComponent {
+export class AppComponent{
   appName = 'JirascopeFE';
   info: any = null;
+  appsList: any = [];
   private subscriptions = new Subscription();
   constructor(
     private http: HttpClient
@@ -36,6 +37,27 @@ export class AppComponent {
         return result
       }));
   }
+
+  getWorkspaceInfoWithJWT() {
+    const JWT = `eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYWlkYSIsImdyb3VwcyI6WyJhZG1pbiIsImRldmVsb3BlciIsInRlc3RlciIsIm9ic2VydmVyIiwicmVwb3J0ZXIiXSwiZXhwIjoxNjg5NDQ5NTYxLCJpYXQiOjE2ODkzNjMxNjEsImlzcyI6IlNlbWFudHljYSIsInN1YiI6ImpvaG4ifQ.MkAEYyPWP5eFcv4ytng1OLuBobB3UVZJHFkHErJyhvOu0RcRMB4Z_3EfzDg0uwMEdiaax3paNw-07QzlGlkBXOhPnEe-iknwiE5pQ3bwpjSdtQOpdeb-vaF1XV9cdpebUOomcpkMM5tL8ZK59MrOsiowkTT5Q3Td5AZNd__0O57IVJQ83GgqSs489uclFuRZTPrqt2dM34ViwLb5ri-PdM3b_Is8We8agIFkW7VBMYkKwYgcqFqQB9lp9vKv_HKi8uTJZWS4u3ocp_DCjZtPh4_OcSXMRw0AHPmA5B1SRyyC4pibVYhklKHHX6xzAXERiy7s7V5my-9-0rhfy5Ht4w`
+    let header = {
+      headers: new HttpHeaders()
+        .set('Authorization',  `Bearer ${JWT}`)
+    }
+    return this.http.get(`http://jirascope.semantyca.com:38707/q/openapi`, header).pipe(
+      map(result => {
+        console.log('result', result);
+        return result
+      }));
+  }
+
+  fakeAuth(){
+    this.subscriptions.add(
+      this.getWorkspaceInfoWithJWT().subscribe(((res: any) => {
+        console.log(res);
+      }))
+    )
+  };
 
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WorkspaceComponent } from './workspace/workspace.component';
 import { HttpClientModule } from '@angular/common/http';
+import {MatButtonModule} from '@angular/material/button'
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,7 +16,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
